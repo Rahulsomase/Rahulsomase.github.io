9127080fadc6e035d6aeca10d434a576d09fc707
Rahul <rahulsomase03@gmail.com>
2022-11-04 12:19:56 +0530

    portfolio

--- style-switcher.js
+++ style-switcher.js
@@ -1,37 +1,4 @@
-//////////// toogel styl switcher////////////////////
 
-// let toogle= document.querySelector(".switch-toggle");
-
-// toogle.addEventListener("click",()=>{
-//   document.querySelector(".style-switch").classList.toggle("open")
-// })
-
-////////hide style swicher scroll tym
-
-// window.addEventListener("scroll",()=>{
-  
-//   if(document.querySelector(".style-switch").classList.contains("open"))
-//   {
-//     document.querySelector(".style-switch").classList.remove("open")
-//   }
-// })
-
-//////////// theme colors////////////////////
-
-// let as=document.querySelectorAll(".alternate-style");
- 
-// const setactive=(color)=>{
-// as.forEach((style)=>{
-//   if(color===style.getAttribute("title")){
-//     style.removeAttribute("disabled");
-//   }
-//   else{
-//     style.setAttribute("disabled","true")
-//   }
-// })
-// }
-
-//////////// theme dark  mode or light mode////////////////////
 
 let day=document.querySelector(".day-night");
 day.addEventListener("click",()=>{
