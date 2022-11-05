ff6b77a775cd448bb63e8ae1fcce15a6f617f247
Rahul <rahulsomase03@gmail.com>
2022-11-05 23:19:47 +0530

    Portfolio

--- script.js
+++ script.js
@@ -1,92 +1,77 @@
-// ///////////////typing animation//////////////////////////////////
-
-var typed= new Typed(".typing",{
-  strings:["Full Stack Java Developer","Back-End Developer"],
-  typeSpeed:80,
-  BackSpeed:60,
-  loop:true
-})
-
-// ///////////////////////Aside//////////////////////////////////
-
-let nav = document.querySelector(".nav")
-
-navlist=nav.querySelectorAll("li")
-
-allsection=document.querySelectorAll(".section")
-
-for(let i=0;i<navlist.length;i++)
-{
-  const a=navlist[i].querySelector("a");
-
-  a.addEventListener("click",function()
-  {
-    for(let i=0;i<allsection.length;i++){
-      allsection[i].classList.remove("back-section")
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
     }
-    for(let j=0;j<navlist.length;j++)
-    {
-      if(navlist[j].querySelector("a").classList.contains("active"))
-      {
-           allsection[j].classList.add("back-section")
-      }
-      navlist[j].querySelector("a").classList.remove("active")
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
     }
-    this.classList.add("active")
-    showSection(this)
-    if(window.innerWidth<1200){
-      asidetoglebtn()
-    }
-  })
-}
+  });
 
-function showSection(element){
-  for(let i=0;i<allsection.length;i++){
-    allsection[i].classList.remove("active")
-  }
-  const target=element.getAttribute("href").split("#")[1]
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-  document.querySelector("#"+target).classList.add("active")
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-}
-function updateNav(element){
-for(let i=0;i<navlist.length;i++){
-  navlist[i].querySelector("a").classList.remove("active")
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-  const target=element.getAttribute("href").split("#")[1]
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Java Backend Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-  if(target===navlist[i].querySelector("a").getAttribute("href").split("#")[1])
-{
-  navlist[i].querySelector("a").classList.add("active")
+  var typed = new Typed(".typing-2", {
+    strings: ["Java Backend Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+    
+  });
 
-}
-}
-}
-document.querySelector(".hire-me").addEventListener("click",function()
-{
-  showSection(this)
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
 
-  updateNav(this)
-})
-// //////////////////////togle btn////////////////////
 
-const toglebtn=document.querySelector(".nav-toggle"),
-
-aside=document.querySelector(".aside")
-
-toglebtn.addEventListener("click",()=>
-{
-  asidetoglebtn()
-})
-
-function asidetoglebtn()
-{
-  aside.classList.toggle("open")
-
-  toglebtn.classList.toggle("open")
-
-  for(let i=0;i<allsection.length;i++)
-  {
-    allsection[i].classList.toggle("open")
-  }
-}
+  
